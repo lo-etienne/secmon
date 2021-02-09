183361c459330f0a01a4999d1c5b18ddf0531472
Robin <r.beaume@student.helmo.be>
2021-02-09 12:58:36 +0100

    Fin d'ecriture des Pattern

--- core/src/main/java/be/flmr/secmon/core/ProtocolPatterns.java
+++ core/src/main/java/be/flmr/secmon/core/ProtocolPatterns.java
@@ -1,12 +1,31 @@
 package be.flmr.secmon.core;
 
+import java.net.PortUnreachableException;
+
 public final class ProtocolPatterns {
     public static final String LETTER = "[A-Za-z]";
-    public static final String DIGIT = "\\d";
-    public static final String LETTER_DIGIT = "[a-zA-Z0-9]";
+    public static final String DIGIT = "[0-9]";
+    public static final String LETTER_DIGIT = LETTER + "|" + DIGIT;
     public static final String CRLF = "\\r\\n";
     public static final String PORT = "([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])";
-    public static final String CHARACTER = "[\\x20-\\xFF]";
+    public static final String CHARACTER = "[\\x20-\\xFF]";//Tous les character imprimable + espace
+    public static final String CHARACTER_PASS = "[\\x21-\\xFF]";//Tous les character imprimable
+    public static final String SP = "[\\x20]";//character espace
+
+    public static final String ID = "(" + LETTER_DIGIT + "){5,10}";
+    public static final String  PROTOCOL = "(" + LETTER_DIGIT + "){3,15}";
+    public static final String  USERNAME = "(" + LETTER_DIGIT + "){3,50}";
+    public static final String  PASSWORD = CHARACTER_PASS + "{3,50}";
+    public static final String  HOST = "(" + LETTER_DIGIT + "|[._-]" + "){3,50}";
+    public static final String  PATH = "/" + "(" + LETTER_DIGIT + "|[\\/._-]" + "){0,100}";
+
+    public static final String  URL = PROTOCOL + "://" + "(" + USERNAME + "(:" + PASSWORD + ")?" + "@)?" + HOST + "(:" + PORT + ")?" + PATH;
+    public static final String  MIN = "(" + DIGIT + "){1,8}";//seuil inférieur (pour l’alarme)
+    public static final String  MAX = "(" + DIGIT + "){1,8}";//seuil supérieur (pour l’alarme)
+    public static final String  FREQUENCY = "(" + DIGIT + "){1,8}";
+    public static final String  AUGMENTED_URL = ID + "!" + URL + "!" + MIN + "!" + MAX + "!" + FREQUENCY;
+    public static final String  STATE = "OK|ALARM|DOWN";
+    public static final String  MESSAGE = "(" + CHARACTER + "){1,200}";
 
     private ProtocolPatterns(){
 

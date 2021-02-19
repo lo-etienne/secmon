9f784f974811b808d5e686940e5f6e2899e53dcf
Robin <r.beaume@student.helmo.be>
2021-02-19 11:21:33 +0100

    Probe router squellette

--- probe/src/main/java/be/flmr/secmon/probe/protocols/ProbeRouter.java
+++ probe/src/main/java/be/flmr/secmon/probe/protocols/ProbeRouter.java
@@ -1,9 +1,26 @@
 package be.flmr.secmon.probe.protocols;
 
+import be.flmr.secmon.core.DaemonProbeProtocolPatterns;
 import be.flmr.secmon.core.router.AbstractRouter;
+import be.flmr.secmon.core.router.Group;
+import be.flmr.secmon.core.router.Protocol;
 import be.flmr.secmon.core.router.Router;
 
 @Router
 public class ProbeRouter extends AbstractRouter {
 
+    @Protocol(pattern = DaemonProbeProtocolPatterns.CONFING)
+    public void config(){
+
+    }
+
+   @Protocol(pattern = DaemonProbeProtocolPatterns.STATE_REQ)
+   public void stateReq(@Group(groupName = "ID") String id){
+
+    }
+
+    @Protocol(pattern = DaemonProbeProtocolPatterns.STATE_RESP)
+    public void stateResp(@Group(groupName = "ID") String id, @Group(groupName = "STATE") String state){
+
+    }
 }

df110b4c5f8a74eadd77afee1a7f3b4ac6efea97
Maxime PRODHOMME <m.prodhomme@student.helmo.be>
2021-03-03 12:16:18 +0100

    (Deuxième commit à cause d'un Bug d'extension qui ne prennait pas en compte les changements au niveau de ServiceThread)

--- probe/src/main/java/be/flmr/secmon/probe/service/ServiceThread.java
+++ probe/src/main/java/be/flmr/secmon/probe/service/ServiceThread.java
@@ -1,2 +1,58 @@
-package be.flmr.secmon.probe.service;public class ServiceThread {
+package be.flmr.secmon.probe.service;
+
+import be.flmr.secmon.core.net.IService;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ServiceThread implements Runnable {
+    private final IService service;
+    private ServiceProber prober;
+
+    private String status = "NOSTATUS";
+
+    private int currentValue;
+
+    public ServiceThread(IService service, ServiceProber prober) {
+        this.service = service;
+        this.prober = prober;
+    }
+
+    @Override
+    public void run() {
+        try {
+            currentValue = prober.get(service);
+            if ((currentValue < service.getMin()) || (currentValue > service.getMax()))
+                status = "ALARM";
+            else
+                status = "OK";
+        } catch (IOException e) {
+            status = "DOWN";
+        }
+    }
+
+    public int getCurrentValue() {
+        return this.currentValue;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ServiceThread serviceThread = (ServiceThread) o;
+        return Objects.equals(this.service, serviceThread.service);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(service);
+    }
+
+    public IService getService() {
+        return this.service;
+    }
 }

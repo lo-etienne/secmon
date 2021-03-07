a8998d516ff70352933986c2a8c923c82dfb067d
Florent <f.raeymaeckes@student.helmo.be>
2021-03-07 17:38:13 +0100

    Fin de travail sur la probe Minecraft

--- probemc/src/main/java/be/flmr/secmon/probemc/service/MinecraftServiceProber.java
+++ probemc/src/main/java/be/flmr/secmon/probemc/service/MinecraftServiceProber.java
@@ -5,101 +5,95 @@ import be.flmr.secmon.core.net.Service;
 import be.flmr.secmon.core.pattern.PatternGroup;
 import be.flmr.secmon.core.pattern.PatternUtils;
 import be.flmr.secmon.probe.service.ServiceProber;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
 
 import java.io.*;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class MinecraftServiceProber implements ServiceProber {
+    private final Gson gson;
 
-    /*
-        mc1!minecraft://mc.hypixel.net/![minimum]![maximum]![frequency]
-     */
-
-    public static void main(String[] args) throws IOException {
-        MinecraftServiceProber minecraftServiceProber = new MinecraftServiceProber();
-        IService service = new Service("mcserv1!minecraft://mc.hypixel.net/abc!12!12!12");
-        minecraftServiceProber.get(service);
-
+    public MinecraftServiceProber() {
+        this.gson = new Gson();
     }
 
     @Override
     public int get(IService service) throws IOException {
         String host = PatternUtils.extractGroup(service.getURL(), PatternGroup.URL, PatternGroup.HOST.name());
         String port = PatternUtils.extractGroup(service.getURL(), PatternGroup.URL, PatternGroup.PORT.name());
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        // PACKET ID
-        writeVarInt(0, buffer);
-        // PROTOCOL VERSION
-        writeVarInt(-1, buffer);
-        // SERVER ADDRESS
-        writeString(service.getURL(), buffer);
-        // SERVER PORT
-        writeShort(port == null ? 25565 : Short.parseShort(port), buffer);
-        // NEXT STATE
-        writeVarInt(1, buffer);
-        try (Socket socket = new Socket(host, port == null ? 25565 : Integer.parseInt(port))) {
+        port = port == null ? "25565" : port;
 
-            int length = buffer.size();
+        try (Socket socket = new Socket(host, Integer.parseInt(port));
+             var in = new DataInputStream(socket.getInputStream());
+             var out = new DataOutputStream(socket.getOutputStream())) {
 
-            writeVarInt(length, socket.getOutputStream());
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DataOutputStream dos = new DataOutputStream(baos);
 
-            byte[] byteArrayFromBuffer = buffer.toByteArray();
-            writeBytes(byteArrayFromBuffer, socket.getOutputStream());
+            dos.writeByte(0x00); // Packet ID
+            writeVarInt(dos, -1); // Protocol Version
+            writeString(dos, host); // Host
+            dos.writeShort(Short.parseShort(port)); // Port
+            writeVarInt(dos, 0x01); // Next State
 
-            socket.getOutputStream().flush();
-            writeVarInt(1, buffer);
-            writeVarInt(0, buffer);
-            socket.getOutputStream().flush();
+            writeVarInt(out, baos.size());
+            out.write(baos.toByteArray());
 
-            int dataLength = readVarInt(socket.getInputStream());
-            int packetId = readVarInt(socket.getInputStream());
-            String data = readString(socket.getInputStream());
+            out.writeByte(0x01); // Length
+            out.writeByte(0x00); // Packet ID
+            out.flush();
 
+            int length = readVarInt(in);
+            if (length <= 0) throw new RuntimeException("Unexpected length");
 
+            int id = readVarInt(in);
+            int strLength = readVarInt(in);
 
-            System.out.println(data);
-        }
-        return 0;
-    }
+            byte[] data = new byte[strLength];
+            in.readFully(data);
 
-    public void writeString(final String temp, final OutputStream outputStream) {
-        byte[] byteArray = temp.getBytes(StandardCharsets.UTF_8);
-        writeVarInt(byteArray.length, outputStream);
-        writeBytes(byteArray, outputStream);
-    }
-
-    public void writeShort(final short temp, final OutputStream outputStream) {
-        try {
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            dataOutputStream.writeShort(temp);
-        } catch (IOException e) {
-            e.printStackTrace();
+            JsonObject object = gson.fromJson(new String(data, StandardCharsets.UTF_8), JsonObject.class);
+            return object.get("players").getAsJsonObject()
+                    .get("online").getAsInt();
         }
     }
 
-    public String readString(final InputStream inputStream) {
-        int length = readVarInt(inputStream);
-        byte[] array = new byte[length];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = readByte(inputStream);
-        }
-        return new String(array, StandardCharsets.UTF_8);
+    public static void writeByte(DataOutputStream buf, byte b) throws IOException {
+        buf.writeByte(b);
     }
 
-    private void writeBytes(byte[] byteArray, OutputStream outputStream) {
-        for (byte b :
-                byteArray) {
-            writeByte(b, outputStream);
-        }
+    public static void writeString(DataOutputStream buf, String str) throws IOException {
+        writeVarInt(buf, str.getBytes(StandardCharsets.UTF_8).length);
+        for (byte b : str.getBytes(StandardCharsets.UTF_8)) writeByte(buf, b);
     }
 
-    public int readVarInt(final InputStream inputStream) {
+    public static void writeVarInt(DataOutputStream buf, int value) throws IOException {
+        do {
+            byte temp = (byte) (value & 0b01111111);
+            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
+            value >>>= 7;
+            if (value != 0) {
+                temp |= 0b10000000;
+            }
+            writeByte(buf, temp);
+        } while (value != 0);
+    }
+
+    public static byte readByte(DataInputStream buf) throws IOException {
+        return buf.readByte();
+    }
+
+    public static int readVarInt(DataInputStream buf) throws IOException {
         int numRead = 0;
         int result = 0;
         byte read;
         do {
-            read = readByte(inputStream);
+            read = readByte(buf);
             int value = (read & 0b01111111);
             result |= (value << (7 * numRead));
 
@@ -112,35 +106,7 @@ public class MinecraftServiceProber implements ServiceProber {
         return result;
     }
 
-    private byte readByte(final InputStream inputStream) {
-        try {
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
-            return dataInputStream.readByte();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return 0;
-    }
-
-    public void writeVarInt(int value, final OutputStream outputStream) {
-        do {
-            byte temp = (byte) (value & 0b01111111);
-            // Note: >>> means that the sign bit is shifted with the rest of the number rather than being left alone
-            value >>>= 7;
-            if (value != 0) {
-                temp |= 0b10000000;
-            }
-            writeByte(temp, outputStream);
-        } while (value != 0);
-    }
-
-    private void writeByte(final byte temp, final OutputStream outputStream) {
-        try {
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            dataOutputStream.writeByte(temp);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public String a(byte[] arr) {
+        return new String(arr, StandardCharsets.UTF_8);
     }
 }
